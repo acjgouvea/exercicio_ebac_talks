1180bff152dd705f8098d45e8cca9212fd66ccf1
carlos gouvea <108358794+acjgouvea@users.noreply.github.com>
2024-03-10 17:04:49 -0300

    criação do js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
-console.log("ola mundo")
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function () {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteEvento = Math.floor(distanciaAteEvento / diaEmMs);
+    const horasAteEvento = Math.floor((distanciaAteEvento % diaEmMs) / horaEmMs);
+    const minutosAteEvento = Math.floor((distanciaAteEvento % horaEmMs) / minutosEmMs);
+    const segundosAteEvento = Math.floor((distanciaAteEvento % minutosEmMs) / 1000);
+
+
+    console.log(distanciaAteEvento);
+    console.log(horasAteEvento);
+    console.log(minutosAteEvento);
+    console.log(segundosAteEvento);
+
+    document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s `;
+if (distanciaAteEvento < 0) {
+    clearInterval(contaAsHoras);
+    document.getElementById('contador').innerHTML = 'Evento expirado!'; 
+}
+
+}, 1000);
